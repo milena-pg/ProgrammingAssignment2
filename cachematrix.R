43ada4aed6d3ed94d4b4578fff65bf40372ada83
milena <milenajo@gmail.com>
2014-11-24 00:25:24 +0100

    commit of R code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions to calculate and make storage for in inverse of a matrix
 
-## Write a short comment describing this function
+
+## For matrix x this function is creating a list of functions to: get the
+## data from x, get the data from stored inverse matrix of x(if there, if not I=NULL),
+## to set the data of x, and to set the inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  I <- NULL
+  set <- function(y) {
+      x <<- y
+      I <<- NULL
+  } 
+  get <- function() x
+  setinverse <- function(Invers) I <<- Invers
+  getinverse <- function() I
+  list(set = set, get = get,
+          setinverse = setinverse,
+          getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+
+## This function takes a list created with func above as an input, calculates
+## the inverse matrix I and returnes it, in case I is not in cache already 
+## and if the given matrix is square
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      I <- x$getinverse()
+      if(!is.null(I)) {     ## checking to see if the stored value for I is NULL
+        message("getting cached data")
+        return(I)
+      }
+      data <- x$get()
+      if(dim(data)[1]==dim(data)[2]) { ## if matrix is not square I will keep value NULL
+        I <- solve(data, ...)
+         x$setinverse(I)
+      }  
+      I
 }
+    
+## Return a matrix that is the inverse of 'x'
